c0ae1c41d83d1e642843ce23a8dd067a8d797546
SeongjunP2 <guppy0217@gmail.com>
2024-04-17 01:40:07 +0900

    update lab15_swing AppMain05

--- lab15_swing/src/com/itwill/swing05/AppMain05.java
+++ lab15_swing/src/com/itwill/swing05/AppMain05.java
@@ -10,6 +10,8 @@ import java.awt.Color;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AppMain05 {
 
@@ -19,6 +21,13 @@ public class AppMain05 {
 	private JTextField textName;
 	private JButton btnNewButton;
 	private JTextArea textArea;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_1_1;
+	
+	String name;
+	String phone;
+	String email;
 
 	/**
 	 * Launch the application.
@@ -52,65 +61,66 @@ public class AppMain05 {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("이름");
+		lblNewLabel = new JLabel("이름");
 		lblNewLabel.setBackground(new Color(255, 255, 255));
 		lblNewLabel.setFont(new Font("굴림", Font.BOLD, 20));
 		lblNewLabel.setBounds(44, 41, 124, 48);
 		frame.getContentPane().add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("전화번호");
+		lblNewLabel_1 = new JLabel("전화번호");
 		lblNewLabel_1.setFont(new Font("굴림", Font.BOLD, 20));
 		lblNewLabel_1.setBounds(44, 118, 124, 48);
 		frame.getContentPane().add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_1_1 = new JLabel("이메일");
+		lblNewLabel_1_1 = new JLabel("이메일");
 		lblNewLabel_1_1.setFont(new Font("굴림", Font.BOLD, 20));
 		lblNewLabel_1_1.setBounds(44, 186, 124, 48);
 		frame.getContentPane().add(lblNewLabel_1_1);
 		
 		textName = new JTextField();
-		name();
 		textName.setColumns(10);
 		textName.setBounds(180, 41, 305, 48);
 		frame.getContentPane().add(textName);
 		
 		textPhone = new JTextField();
-		phone();
 		textPhone.setColumns(10);
 		textPhone.setBounds(180, 118, 305, 48);
 		frame.getContentPane().add(textPhone);
 		
 		textEmail = new JTextField();
-		email();
 		textEmail.setColumns(10);
 		textEmail.setBounds(180, 186, 305, 48);
 		frame.getContentPane().add(textEmail);
 		
 		btnNewButton = new JButton("입력");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				handleInputButtonClick();
+			}
+		});
 		btnNewButton.setFont(new Font("굴림", Font.BOLD, 20));
 		btnNewButton.setBounds(526, 186, 145, 48);
 		frame.getContentPane().add(btnNewButton);
 		
 		textArea = new JTextArea();
 		textArea.setFont(new Font("Monospaced", Font.PLAIN, 16));
-		textArea.setText("이름:\r\n" + "전화번호:\r\n" + "이메일:");
 		textArea.setBounds(44, 268, 627, 130);
 		frame.getContentPane().add(textArea);
 	}
 	
-	private void name() {
-		String name = textName.getText();
-		textArea.setText(name);
-	}
-	
-	private void phone() {
-		String phone = textPhone.getText();
-		textArea.setText(phone);
-	}
-	
-	private void email() {
-		String email = textPhone.getText();
-		textArea.setText(email);
+	private void handleInputButtonClick() {
+		name = "이름: " + textName.getText() + "\n";
+		phone = "번호: " + textPhone.getText() + "\n";
+		email = "이메일: " + textEmail.getText() + "\n";
+		
+		String result = name + phone + email;
+		
+		textArea.setText(result);
+		
+		textName.setText("");
+		textPhone.setText("");
+		textEmail.setText("");
+		
 	}
 
 }
