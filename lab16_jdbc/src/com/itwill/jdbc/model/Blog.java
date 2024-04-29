5a94c768de9820bec65f929caf327e12f081c99b
SeongjunP2 <guppy0217@gmail.com>
2024-04-29 18:18:30 +0900

    update lab16_jdbc crud swing project

--- lab16_jdbc/src/com/itwill/jdbc/model/Blog.java
+++ lab16_jdbc/src/com/itwill/jdbc/model/Blog.java
@@ -3,88 +3,91 @@ package com.itwill.jdbc.model;
 import java.time.LocalDateTime;
 
 public class Blog {
-	public static final class Entify {
-		// 데이터베이스 BLOGS 테이블의 컬럼 이름들을 상수로 선언.
-		public static final String COL_ID = "ID";
-		public static final String COL_TITLE = "TITLE";
-		public static final String COL_CONTENT = "CONTENT";
-		public static final String COL_WRITER = "WRITER";
-		public static final String COL_CREATED_TIME = "CREATED_TIME";
-		public static final String COL_MODIFIED_TIME = "MODIFIED_TIME";
-	}
-	
-	private int id;
-	private String title;
-	private String content;
-	private String writer;
-	private LocalDateTime createdTime;
-	private LocalDateTime modifiedTime;
-	
-	public Blog() {}
+    
+    public static final class Entity {
+        // 데이터베이스 테이블 이름을 상수로 선언. 
+        public static final String TBL_BLOGS = "BLOGS";
 
-	public Blog(int id, String title, String content, String writer, LocalDateTime createdTime,
-			LocalDateTime modifiedTime) {
-		this.id = id;
-		this.title = title;
-		this.content = content;
-		this.writer = writer;
-		this.createdTime = createdTime;
-		this.modifiedTime = modifiedTime;
-	}
+        // 데이터베이스 BLOGS 테이블의 컬럼 이름들을 상수로 선언.
+        public static final String COL_ID = "ID";
+        public static final String COL_TITLE = "TITLE";
+        public static final String COL_CONTENT = "CONTENT";
+        public static final String COL_WRITER = "WRITER";
+        public static final String COL_CREATED_TIME = "CREATED_TIME";
+        public static final String COL_MODIFIED_TIME = "MODIFIED_TIME";
+    }
+    
+    private int id;
+    private String title;
+    private String content;
+    private String writer;
+    private LocalDateTime createdTime;
+    private LocalDateTime modifiedTime;
+    
+    public Blog() {}
 
-	public int getId() {
-		return id;
-	}
+    public Blog(int id, String title, String content, String writer, LocalDateTime createdTime,
+            LocalDateTime modifiedTime) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.createdTime = createdTime;
+        this.modifiedTime = modifiedTime;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public String getContent() {
-		return content;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    public String getContent() {
+        return content;
+    }
 
-	public String getWriter() {
-		return writer;
-	}
+    public void setContent(String content) {
+        this.content = content;
+    }
 
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
+    public String getWriter() {
+        return writer;
+    }
 
-	public LocalDateTime getCreatedTime() {
-		return createdTime;
-	}
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
 
-	public void setCreatedTime(LocalDateTime createdTime) {
-		this.createdTime = createdTime;
-	}
+    public LocalDateTime getCreatedTime() {
+        return createdTime;
+    }
 
-	public LocalDateTime getModifiedTime() {
-		return modifiedTime;
-	}
+    public void setCreatedTime(LocalDateTime createdTime) {
+        this.createdTime = createdTime;
+    }
 
-	public void setModifiedTime(LocalDateTime modifiedTime) {
-		this.modifiedTime = modifiedTime;
-	}
+    public LocalDateTime getModifiedTime() {
+        return modifiedTime;
+    }
 
-	@Override
-	public String toString() {
-		return "Blog [id=" + id + ", title=" + title + ", content=" + content + ", writer=" + writer + ", createdTime="
-				+ createdTime + ", modifiedTime=" + modifiedTime + "]";
-	}
-	
+    public void setModifiedTime(LocalDateTime modifiedTime) {
+        this.modifiedTime = modifiedTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Blog [id=" + id + ", title=" + title + ", content=" + content + ", writer=" + writer + ", createdTime="
+                + createdTime + ", modifiedTime=" + modifiedTime + "]";
+    }
 
 }
